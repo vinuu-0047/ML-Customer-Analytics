c70a2df829caa3432c7318f97824914e8d07e2f9
Vinayak <vinayakaswal927@gmail.com>
2026-06-10 11:24:13 +0530

    add redirect

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 from analytics.views import dashboard_view
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', dashboard_view, name='dashboard'), # This points the homepage to your AI view!
+    path('', RedirectView.as_view(url='/analytics/')), # Change /analytics/ to your actual app path
 ]
